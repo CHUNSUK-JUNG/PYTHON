649fe11d974eabe698b9c3d4433d83f585f1a769
CHUNSUK, JUNG(SCHOOL) <d903613@nate.com>
2021-04-14 16:48:17 +0900

    STUDY6 중간 완료

---
+++ 0.STUDY/STUDY6-파이썬 필수 라이브러리/구조체.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct { 
+    double v; 
+    int t; 
+    char c;
+} save_type;
+
+int main() {
+    save_type s = {7.5f, 15, 'A'};
+    FILE *f = fopen("output", "w");
+    fwrite(&s, sizeof(save_type), 1, f);
+    fclose(f);
+    return 0;
+}
